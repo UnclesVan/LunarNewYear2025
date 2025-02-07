f58a3b6e221d57a556b6ad432636a24cbc6ce792
UnclesVan <mr.gremlingaming@gmail.com>
2025-02-07 17:42:11 +1100

    Update AdoPt-Me!.lua

--- AdoPt-Me!.lua
+++ AdoPt-Me!.lua
@@ -138,6 +138,9 @@ local function collectSpecialStar()
         -- Collecting special stars
         local argsMoonInterior = {"MoonInterior", "13", true}
         ShootingStarCollected:FireServer(unpack(argsMoonInterior))
+
+        local argsMoonInterior = {"MoonInterior", "26", true}
+        ShootingStarCollected:FireServer(unpack(argsMoonInterior))
         
         local argsMainMap1 = {"MainMap", "111", true}
         ShootingStarCollected:FireServer(unpack(argsMainMap1))
@@ -145,12 +148,15 @@ local function collectSpecialStar()
         local argsMainMap2 = {"MainMap", "81", true}  -- Adding special star with ID 81
         ShootingStarCollected:FireServer(unpack(argsMainMap2))
 
+       
+
         specialStarCount = specialStarCount + 3  -- Increment for all three stars collected
         specialStarsLabel.Text = "Special Stars Count: " .. tostring(specialStarCount)
 
         -- Update collecting text for the new special star
         updateCollectingText("MainMap", "81")
         updateCollectingText("MoonInterior", "13")
+        updateCollectingText("MoonInterior", "26")
         wait(10)
     end
 end
